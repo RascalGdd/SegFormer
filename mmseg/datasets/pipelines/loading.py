9b74bfebe64bcd10f0a93f0a608e1ac603adc387
Diandian Guo <guodiandian1998@163.com>
2023-04-10 19:06:07 +0200

    debug

--- mmseg/datasets/pipelines/loading.py
+++ mmseg/datasets/pipelines/loading.py
@@ -99,8 +99,8 @@ class LoadImageFromFile(object):
         # add vanishing_mask here!
 
         vanishing_mask = vanishing_point_to_depth_mask(None, (img.shape[0], img.shape[1]))
-        results["img"] = np.concatenate((results["img"], vanishing_mask[0]), axis=0)
-
+        results["img"] = np.concatenate((results["img"], vanishing_mask.transpose(0, 2, 1)), axis=0)
+        asd
         return results
 
     def __repr__(self):
