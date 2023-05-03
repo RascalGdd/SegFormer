32da0e1980e33ef0ffbdfc16da2d6d4d2888ec97
tonglu <tonglu@ethz.student.ch>
2023-05-03 17:52:31 +0200

    setup hrda

--- mmseg/models/utils/__init__.py
+++ mmseg/models/utils/__init__.py
@@ -1,11 +1,10 @@
 from .inverted_residual import InvertedResidual, InvertedResidualV3
 from .make_divisible import make_divisible
-from .res_layer import ResLayer
 from .self_attention_block import SelfAttentionBlock
 from .up_conv_block import UpConvBlock
 
 __all__ = [
-    'ResLayer', 'SelfAttentionBlock', 'make_divisible', 'InvertedResidual',
+    'SelfAttentionBlock', 'make_divisible', 'InvertedResidual',
     'UpConvBlock', 'InvertedResidualV3', 
     'mit_convert', 'nchw_to_nlc', 'nlc_to_nchw'
 ]
