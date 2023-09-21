4ce7d2750fabbfc16e8564c76b6b190b4f9bba74
tonglu <tonglu@ethz.student.ch>
2023-09-21 16:00:17 +0200

    acdc 512

--- local_configs/segformer/B3/segformer.b3.512x512.citycoarse.160k.py
+++ local_configs/segformer/B3/segformer.b3.512x512.citycoarse.160k.py
@@ -1,6 +1,6 @@
 _base_ = [
     '../../_base_/models/segformer.py',
-    '../../_base_/datasets/acdc_512x512_repeat.py',
+    '../../_base_/datasets/cityscapes_coarse_512x512_repeat.py',
     '../../_base_/default_runtime.py',
     '../../_base_/schedules/schedule_160k_adamw.py'
 ]
